8dee102573e56dc9652e948ffd039723e3cc31e4
neby55 <ben@progweb.fr>
2017-05-31 09:04:05 +0200

    abstract Cart & Vouncher

--- app/models/phalconmerce/popo/abstracts/AbstractCart.php
+++ app/models/phalconmerce/popo/abstracts/AbstractCart.php
@@ -4,6 +4,81 @@ namespace Phalconmerce\Popo\Abstracts;
 
 use Phalconmerce\AbstractModel;
 
-
 abstract class AbstractCart extends AbstractModel {
+
+	/**
+	 * @Primary
+	 * @Identity
+	 * @Column(type="integer", nullable=false)
+	 * @var int
+	 */
+	public $id;
+
+	/**
+	 * @Primary
+	 * @Column(type="integer", nullable=false)
+	 * @var int
+	 */
+	public $fk_cart_id;
+
+	/**
+	 * @Primary
+	 * @Column(type="integer", nullable=false)
+	 * @var int
+	 */
+	public $fk_order_id;
+
+	/**
+	 * @Primary
+	 * @Column(type="integer", nullable=false)
+	 * @var int
+	 */
+	public $fk_currency_id;
+
+	/**
+	 * @Primary
+	 * @Column(type="integer", nullable=false)
+	 * @var int
+	 */
+	public $fk_product_id;
+
+	/**
+	 * @Primary
+	 * @Column(type="integer", nullable=false)
+	 * @var int
+	 */
+	public $fk_promotion_id;
+
+	/**
+	 * @Column(type="integer", length=4, nullable=false, default=1)
+	 * @Index
+	 * @var int
+	 */
+	public $quantity;
+
+	/**
+	 * @Column(type="float", nullable=false)
+	 * @var float
+	 */
+	public $amountVatExcluded;
+
+	/**
+	 * @Column(type="float", nullable=false)
+	 * @var float
+	 */
+	public $amountVatIncluded;
+
+	/**
+	 * @return AbstractMenu
+	 */
+	public function getParent() {
+		return \Phalconmerce\Popo\Cart::findFirst($this->getParentId());
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getParentId() {
+		return $this->fk_cart_id;
+	}
 }
--- app/models/phalconmerce/popo/abstracts/AbstractVouncher.php
+++ app/models/phalconmerce/popo/abstracts/AbstractVouncher.php
@@ -4,7 +4,62 @@ namespace Phalconmerce\Popo\Abstracts;
 
 use Phalconmerce\AbstractModel;
 
-
 abstract class AbstractVouncher extends AbstractModel {
 
+	/**
+	 * @Primary
+	 * @Identity
+	 * @Column(type="integer", nullable=false)
+	 * @var int
+	 */
+	public $id;
+
+	/**
+	 * @Column(type="string", length=64, nullable=false)
+	 * @var string
+	 * @Index
+	 */
+	public $code;
+
+	/**
+	 * @Column(type="integer", length=2, nullable=false, default=0)
+	 * @var int
+	 */
+	public $type;
+
+	/**
+	 * @Column(type="timestamp", nullable=false)
+	 * @var string
+	 */
+	public $startDate;
+
+	/**
+	 * @Column(type="timestamp", nullable=false)
+	 * @var string
+	 */
+	public $endDate;
+
+	/**
+	 * @Column(type="timestamp", nullable=false)
+	 * @var string
+	 */
+	public $useDate;
+
+	/**
+	 * @Column(type="float", nullable=false)
+	 * @var float
+	 */
+	public $amountVatExcluded;
+
+	/**
+	 * @Column(type="float", nullable=false)
+	 * @var float
+	 */
+	public $amountMinimumVatExcluded;
+
+	/**
+	 * @Column(type="integer", length=2, nullable=false, default=0)
+	 * @var int
+	 */
+	public $status;
 }
