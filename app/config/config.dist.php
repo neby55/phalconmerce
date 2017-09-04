4b5fa9334c403722511a5de3df14e44a9763908e
neby55 <ben@progweb.fr>
2017-09-04 11:06:24 +0200

    adding a config file template

---
+++ app/config/config.dist.php
@@ -0,0 +1,22 @@
+<?php
+
+defined('APP_PATH') || define('APP_PATH', realpath('.'));
+
+return new \Phalcon\Config(array(
+	'database' => array(
+		'adapter' => '',
+		'host' => '',
+		'username' => '',
+		'password' => '',
+		'dbname' => '',
+		'charset' => 'utf8',
+	),
+	'cacheDir' => APP_PATH . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'cache' . DIRECTORY_SEPARATOR,
+	'baseUri' => isset($_SERVER['BASE_URI']) ? $_SERVER['BASE_URI'] : '', // BASE_URI index generated thanks to public/.htaccess
+	'adminDir' => 'admin',
+	'adminTitle' => 'Phalconmerce',
+	'adminTheme' => 'sb-admin2',
+	'apiEnabled' => true,
+	'apiDir' => 'api',
+	'apiCorsAllowOrigin' => array('localhost'), // set an array of allowed domains, or just the string *
+));
