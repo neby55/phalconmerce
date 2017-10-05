af7073835bdea2bc48bb292823ffccc00286ac4a
neby55 <ben@progweb.fr>
2017-10-05 18:26:50 +0200

    setting metas to frontendService from Url object

--- app/frontend/controllers/abstracts/AbstractUrlController.php
+++ app/frontend/controllers/abstracts/AbstractUrlController.php
@@ -14,7 +14,13 @@ use Frontend\Controllers\ControllerBase;
 
 class AbstractUrlController extends ControllerBase {
 	public function dispatcherAction() {
+		/** @var \Phalconmerce\Models\Popo\Abstracts\AbstractUrl $urlObject */
 		$urlObject = $this->dispatcher->getParam('url');
+
+		// Defines META
+		$this->getDI()->get('frontendService')->setMetaTitle($urlObject->metaTitle);
+		$this->getDI()->get('frontendService')->setMetaDescription($urlObject->metaDescription);
+		$this->getDI()->get('frontendService')->setMetaKeywords($urlObject->metaKeywords);
 
 		$this->dispatcher->forward(
 			[
